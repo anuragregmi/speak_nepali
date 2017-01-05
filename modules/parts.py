c273b9fabfb72afbb67dde786b544308bb3d31e4
Anurag Regmi <anurag1234565@gmail.com>
2017-01-05 23:08:51 +0545

    file updated

--- modules/parts.py
+++ modules/parts.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#vim: set fileencoding: utf-8
 import sys
 
 def part(string):
@@ -10,8 +11,13 @@ def part(string):
     count = 0
     st = ""
     for char in string:
-        st += char
-        count += 1
+        if char == " ":
+            characters.append(char)
+            count = 0
+        else:
+            st += char
+            count += 1
+        
         if count == 3:
             characters.append(st)
             st = ""
@@ -19,7 +25,28 @@ def part(string):
 
     return characters
 
+def sabdaharu(characters):
+    """
+    returns list of "shabda" of characters
+    """
+    letters = ["क", "ख", "ग", "घ", "ड॒", "च", "छ", "ज", "झ", "ञ", "ट", "ठ", "ड", "ढ", "ण", "त", "थ" ,"द", "ध", "न", "प", "फ", "ब", "भ", "म", "य", "र", "ल", "व", "श", "ष", "स", "ह", "अ", "आ", "इ", "ई", "उ", "ऊ", "ए", "ऐ", "ओ" ]
+    shabda = []
+    st = ""
+    for ch in characters:
+        if ch == " ":
+            shabda.append(ch)
+        else:
+            if ch in letters:
+                shabda.append(st)
+                st = ch
+            else:
+                st += ch
+    shabda.append(st)
+    shabda.pop(0)
+    return  shabda
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print  part(sys.argv[1])
+    s = sabdaharu(part(sys.argv[1]))
+    for char in s:
+        print "[ "+char+" ]",
